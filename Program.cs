74e69dd3ffc3092b8ef9fcdff3d5722dec6386ff
sammy <ehuerikenbaba@gmail.com>
2021-06-22 22:14:04 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CsharpFundamental
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            const int passwordLength = 10;
+
+            var buffer = new char[passwordLength];
+            for (var i = 0; i < passwordLength; i++)
+                buffer[i] = (char)('a' + random.Next(0, 26));
+            var password = new string(buffer);
+            Console.WriteLine(password);
+           
+        }
+    }
+}
